7c9ee57553e4e4b7a87777b59a1db6fa347c69ab
임승환 <>
2020-03-11 15:06:25 +0900

    first commit

---
+++ app/src/main/java/kr/shlim/roomexample/TodoDao.kt
@@ -0,0 +1,19 @@
+package kr.shlim.roomexample
+
+import androidx.room.*
+
+@Dao
+interface TodoDao {
+
+    @Query("SELECT * FROM Todo")
+    fun getAll() : List<Todo>
+
+    @Insert
+    fun insert(todo : Todo)
+
+    @Update
+    fun update(todo : Todo)
+
+    @Delete
+    fun delete(todo : Todo)
+}
