e1760986bafa1559e2b25769eb9402a67b9d6d2f
shlim <shlim@chunjae.co.kr>
2020-04-20 10:54:50 +0900

    coroutine사용하여 allowMainThreadQueries()  제거

--- app/src/main/java/kr/shlim/roomexample/MainActivity.kt
+++ app/src/main/java/kr/shlim/roomexample/MainActivity.kt
@@ -3,8 +3,11 @@ package kr.shlim.roomexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,16 +18,16 @@ class MainActivity : AppCompatActivity() {
         val db = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java, "todo-db"
-        ).allowMainThreadQueries()
-         .build()
-
+        ).build()
 
         db.todoDao().getAll().observe(this, Observer {
             todos-> result_text.text=todos.toString()
         })
 
         add_button.setOnClickListener() {
-            db.todoDao().insert(Todo(todo_edit.text.toString()))
+            lifecycleScope.launch(Dispatchers.IO) {
+                db.todoDao().insert(Todo(todo_edit.text.toString()))
+            }
         }
     }
 }
