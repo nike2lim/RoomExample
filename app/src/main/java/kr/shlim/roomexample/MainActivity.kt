c3aebe24d9c2c2bbd2b59c38e34e77a4e46eb679
shlim <shlim@chunjae.co.kr>
2020-04-20 11:25:56 +0900

    viewModel 적용

--- app/src/main/java/kr/shlim/roomexample/MainActivity.kt
+++ app/src/main/java/kr/shlim/roomexample/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.shlim.roomexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
@@ -11,22 +12,19 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    val viewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "todo-db"
-        ).build()
-
-        db.todoDao().getAll().observe(this, Observer {
+        viewModel.getAll().observe(this, Observer {
             todos-> result_text.text=todos.toString()
         })
 
         add_button.setOnClickListener() {
             lifecycleScope.launch(Dispatchers.IO) {
-                db.todoDao().insert(Todo(todo_edit.text.toString()))
+                viewModel.insert(Todo(todo_edit.text.toString()))
             }
         }
     }
